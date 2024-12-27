ccc7c676240decf0c4592c2f98556974dd621a6e
sasobapriyanjana11 <sasoba.zxc@gmail.com>
2024-12-27 21:18:25 +0530

    implement Item update using Reducer functions

--- src/Pages/UpdateItem.tsx
+++ src/Pages/UpdateItem.tsx
@@ -1,12 +1,32 @@
+import {useNavigate} from "react-router";
+import {useContext, useState} from "react";
+import {ItemContext} from "../store/ItemProvider.tsx";
+import {Item} from "../models/Item.ts";;
+import {ItemModal} from "../component/ItemModal.tsx";
+
 export const UpdateItem = () => {
+
+    const navigate = useNavigate();
+    const [items, dispatch] = useContext(ItemContext);
+
+    const [code, setCode] = useState("");
+    const [name, setName] = useState("");
+    const [price, setPrice] = useState<number>(0);
+    const [quantity, setQuantity] = useState<number>(0);
+
+
+    function handleSubmit() {
+        const newItem= new Item(code, name, price, quantity);
+        dispatch({type: 'UPDATE_ITEM', payload: newItem});
+        navigate('/');
+    }
+
     return (
         <>
-            <h1>Update Item</h1>
-            <input type="text" placeholder="Item Code" />
-            <input type="text" placeholder="New Name" />
-            <input type="number" placeholder="New Price" />
-            <input type="number" placeholder="New Quantity" />
-            <button>Update</button>
+            <header><h2>Update Item</h2></header>
+            <br/>
+            <ItemModal handleSubmit={handleSubmit} setCode={setCode} setName={setName} setPrice={setPrice}
+                       setQuantity={setQuantity}>Delete Item</ItemModal>
         </>
     );
 };
