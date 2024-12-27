6058bbe48b0363aaeef4a0286cf40224c3bed429
sasobapriyanjana11 <sasoba.zxc@gmail.com>
2024-12-27 21:18:10 +0530

    implement Item delete using Reducer functions

--- src/Pages/DeleteItem.tsx
+++ src/Pages/DeleteItem.tsx
@@ -1,9 +1,34 @@
+import {useNavigate} from "react-router";
+import {useContext, useState} from "react";
+import {ItemContext} from "../store/ItemProvider.tsx";
+import {Item} from "../models/Item.ts";
+import {ItemModal} from "../component/ItemModal.tsx";
+
 export const DeleteItem = () => {
+
+    const navigate = useNavigate();
+    const [items, dispatch] = useContext(ItemContext);
+
+    const [code, setCode] = useState("");
+    const [name, setName] = useState("");
+    const [price, setPrice] = useState<number>(0);
+    const [quantity, setQuantity] = useState<number>(0);
+
+
+    function handleSubmit() {
+        const newItem = new Item(code, name, price, quantity);
+        dispatch({type: 'DELETE_ITEM', payload: newItem});
+        navigate('/');
+    }
+
     return (
         <>
-            <h1>Delete Item</h1>
-            <input type="text" placeholder="Item Code" />
-            <button>Delete</button>
+            <header><h2>Delete Item</h2></header>
+            <br/>
+
+            <ItemModal handleSubmit={handleSubmit} setCode={setCode} setName={setName} setPrice={setPrice}
+                   setQuantity={setQuantity}>Delete Item</ItemModal>
         </>
+
     );
 };
